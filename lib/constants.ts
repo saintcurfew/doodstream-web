0e35bd73cbdc14ece4254d7c17ccaa6d4c6018fd
saintcurfew <78733315+saintcurfew@users.noreply.github.com>
2024-03-31 17:08:40 +0530

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const DOODSTREAM_BASE_URL =
     process.env.DOODSTREAM_BASE_URL || "https://doodapi.com";
 export const DOODSTREAM_API_KEY = process.env.DOODSTREAM_API_KEY;
-export const SITENAME = process.env.SITENAME || "DoodWeb";
-export const DEFAULT_PER_PAGE = parseInt(process.env.DEFAULT_PER_PAGE || "40");
+export const SITENAME = process.env.SITENAME || "KoreanHub";
+export const DEFAULT_PER_PAGE = parseInt(process.env.DEFAULT_PER_PAGE || "50");
 export const DEFAULT_REVALIDATE_INTERVAL = 3600;
